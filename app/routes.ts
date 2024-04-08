3f6d7efb9ea39c5602e799e265e2a72637cfcb9a
Thomas Bouillon <tbouillon54@gmail.com>
2024-04-08 21:05:14 +0200

    fix: routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,18 +1,18 @@
 export default function routes() {
   return {
     home: () => "/",
-    news: () => "/actualites",
-    hunters: () => "/conducteurs",
+    news: () => "/actualites.html",
+    hunters: () => "/conducteurs.html",
     newOrUpdateHunter: () =>
-      "/conducteurs/devenir-conducteur-ou-demande-agreement",
-    cynophilie: () => "/cynophilie",
-    shop: () => "/boutique",
-    delegations: () => "/delegations-et-assocations-departementales",
-    downloads: () => "/divers-et-telechargements",
-    guarantee: () => "/fonds-de-garantie",
-    geschasse: () => "/geschasse",
-    newsPaper: () => "/jusquau-bout-la-revue",
-    schedules: () => "/stages-unucr",
-    links: () => "/liens",
+      "/conducteurs/devenir-conducteur-ou-demande-agreement.html",
+    cynophilie: () => "/cynophilie.html",
+    shop: () => "/boutique.html",
+    delegations: () => "/delegations-et-assocations-departementales.html",
+    downloads: () => "/divers-et-telechargements.html",
+    guarantee: () => "/fonds-de-garantie.html",
+    geschasse: () => "/geschasse.html",
+    newsPaper: () => "/jusquau-bout-la-revue.html",
+    schedules: () => "/stages-unucr.html",
+    links: () => "/liens.html",
   };
 }
